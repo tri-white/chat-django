2a5e46b6f1bc8e2f512c7e2438092e628b478436
tri-white <olegovic101@gmail.com>
2023-12-06 19:22:02 +0200

    project with registration and authorization

---
+++ accounts/models.py
@@ -0,0 +1,9 @@
+# accounts/models.py
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.username
---
+++ accounts/views.py
@@ -0,0 +1,28 @@
+# accounts/views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout
+from .forms import CustomUserCreationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Change 'home' to the name of your home page
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  # Change 'home' to the name of your home page
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
