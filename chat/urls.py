06adf6f42ce1e676c9160606929da5991e8e6329
tri-white <olegovic101@gmail.com>
2023-12-06 19:52:27 +0200

    can send messages. but displays only users who have been messaged atleast once

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,9 @@
 # chat/urls.py
 from django.urls import path
-from .views import chat_home, chat_with_user
+from .views import chat_home, chat_with_user, send_message
 
 urlpatterns = [
     path('home/', chat_home, name='chat_home'),
     path('user/<int:user_id>/', chat_with_user, name='chat_with_user'),
+    path('send_message/<int:user_id>/', send_message, name='send_message'),  # Add this line
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,8 +1,32 @@
 # chat/views.py
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.db import models
 from .models import Message, UserStatus
+from accounts.models import CustomUser
+from django.http import HttpResponseBadRequest
+from django.core.exceptions import ObjectDoesNotExist  # Add this import
 
+@login_required
+def send_message(request, user_id):
+    if request.method == 'POST':
+        content = request.POST.get('content')
+        if content:
+            sender = request.user
+            try:
+                receiver_status = UserStatus.objects.get(user__id=user_id)
+                receiver = receiver_status.user
+            except ObjectDoesNotExist:
+                # If UserStatus does not exist, create it
+                receiver = UserStatus.objects.create(user_id=user_id).user
+
+            message = Message.objects.create(sender=sender, receiver=receiver, content=content)
+            return redirect('chat_with_user', user_id=user_id)
+        else:
+            return HttpResponseBadRequest("Message content cannot be empty.")
+    else:
+        return HttpResponseBadRequest("Invalid request method.")
+    
 @login_required
 def chat_home(request):
     users = UserStatus.objects.exclude(user=request.user)
